931552948859cf81db57258599376fde93871faa
xyznn <hjseo0819@dankook.ac.kr>
2026-04-04 18:56:20 +0900

    [ Solution ] Step 3

---
+++ step/step3/25304_receipt.py
@@ -0,0 +1,13 @@
+total = int(input())
+n = int(input())
+
+sum = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    sum += a*b
+
+if total == sum:
+    print("Yes")
+else:
+    print("No")
